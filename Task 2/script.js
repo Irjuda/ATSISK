0fbeb9581077154690557e9e87574502cb09ea1a
Irjuda <irjuda@gmail.com>
2022-07-07 23:25:27 +0300

    Task 2 added check ignores

--- Task 2/script.js
+++ Task 2/script.js
@@ -1,4 +1,3 @@
-// @ts-nocheck
 /* ------------------------------ TASK 2 ----------------------------
 Parašykite JS kodą, kuris skaičiuos kiek kartų buvo paspaustas mygtukas
 su tekstu "CLICK ME". Paspaudimų rezultatas turi būti matomas dešinėje
@@ -7,10 +6,12 @@ pusėje esančiame "state" skaičiavimo bloke (<div id="btn__state">0</div>)
 let btn = document.querySelector('#btn__element')
 let click = 0
 
+// @ts-ignore
 btn.addEventListener('click', () => {
     
     click += 1;
 
+    // @ts-ignore
     document.querySelector('#btn__state').innerHTML = click;
     
 })
